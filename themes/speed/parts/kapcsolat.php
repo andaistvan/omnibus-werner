ea74c48b77ad8739ee67e01615f0965cc76eb35a
andaistvan <andaistvan@gmail.com>
2016-06-21 16:11:38 +0200

    kapcsolat - ninja form ready
    ninja form ready

--- themes/speed/parts/kapcsolat.php
+++ themes/speed/parts/kapcsolat.php
@@ -29,22 +29,89 @@
 
       </div><!-- main-container kapcsolat-data-cont -->
 
-      <div id="kapcsolat-form-cont">
-         <div class="main-container">
+      <div id="kapcsolat-form-cont"><!-- kapcsolat-form-cont -->
+         <div class="main-container"><!-- main-container  -->
 
-            <div class="medium-4 columns">
+            <div id="ninja_forms_form_5_cont" class="ninja-forms-cont">
+                <div id="ninja_forms_form_5_wrap" class="ninja-forms-form-wrap">
+                    <div id="ninja_forms_form_5_response_msg" style="" class="ninja-forms-response-msg "></div>
+                    <form id="ninja_forms_form_5" enctype="multipart/form-data" method="post" name="" action="" class="ninja-forms-form">
 
-            </div>
+                        <input type="hidden" id="_wpnonce" name="_wpnonce" value="f2e39930c7" />
+                        <input type="hidden" name="_wp_http_referer" value="/omnibus-werner/" />
+                        <input type="hidden" name="_ninja_forms_display_submit" value="1">
+                        <input type="hidden" name="_form_id" id="_form_id" value="5">
+                        <!-- <div class="hp-wrap">
+                           <label>If you are a human and are seeing this field, please leave it blank.
+                                <input type="text" value="" name="_wNRb2">
+                                <input type="hidden" value="_wNRb2" name="_hp_name">
+                            </label>
+                        </div> -->
+                        <div id="ninja_forms_form_5_all_fields_wrap" class="ninja-forms-all-fields-wrap">
+                            <div class="ninja-forms-required-items">A csillaggal jelölt mezők kitöltése kötelező!</div>
 
-            <div class="medium-4 columns">
+                           <div class="form-first-row-cont">
+                           <div class="medium-4 columns"><!-- név -->
+                              <div class="field-wrap text-wrap label-inside" id="ninja_forms_field_5_div_wrap" data-visible="1">
+                                  <input type="hidden" id="ninja_forms_field_5_type" value="text">
+                                  <input id="ninja_forms_field_5" data-mask="" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="character(s) left" name="ninja_forms_field_5" type="text" placeholder="" class="ninja-forms-field  ninja-forms-req " value="Név *" rel="5"
+                                  />
+                                  <input type="hidden" id="ninja_forms_field_5_label_hidden" value="Név *">
+                                  <div id="ninja_forms_field_5_error" style="display:none;" class="ninja-forms-field-error">
+                                  </div>
+                              </div>
+                           </div>
 
-            </div>
+                           <div class="medium-4 columns"><!-- e-mail -->
+                              <div class="field-wrap text-wrap label-inside" id="ninja_forms_field_7_div_wrap" data-visible="1">
+                                  <input type="hidden" id="ninja_forms_field_7_type" value="text">
+                                  <input id="ninja_forms_field_7" data-mask="" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="" name="ninja_forms_field_7" type="text" placeholder="" class="ninja-forms-field  ninja-forms-req email " value="E-mail *" rel="7" />
+                                  <input type="hidden" id="ninja_forms_field_7_label_hidden" value="E-mail *">
+                                  <div id="ninja_forms_field_7_error" style="display:none;" class="ninja-forms-field-error">
+                                  </div>
+                              </div>
+                           </div>
 
-            <div class="medium-4 columns">
+                           <div class="medium-4 columns"><!-- telefon -->
+                              <div class="field-wrap text-wrap label-inside" id="ninja_forms_field_8_div_wrap" data-visible="1">
+                                  <input type="hidden" id="ninja_forms_field_8_type" value="text">
+                                  <input id="ninja_forms_field_8" data-mask="" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="" name="ninja_forms_field_8" type="text" placeholder="" class="ninja-forms-field  " value="Telefon" rel="8" />
+                                  <input type="hidden" id="ninja_forms_field_8_label_hidden" value="Telefon">
+                                  <div id="ninja_forms_field_8_error" style="display:none;" class="ninja-forms-field-error">
+                                  </div>
+                              </div>
+                           </div>
+                        </div> <!-- form-first-row-cont -->
 
-            </div>
+                           <div class="small-12 columns"><!-- üzenet - submit -->
+                              <div class="field-wrap textarea-wrap label-inside" id="ninja_forms_field_9_div_wrap" data-visible="1">
+                                  <input type="hidden" id="ninja_forms_field_9_type" value="textarea">
+                                  <textarea name="ninja_forms_field_9" id="ninja_forms_field_9" class="ninja-forms-field  ninja-forms-req" rel="9" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="character(s) left">Üzenet *</textarea>
+                                  <input type="hidden" id="ninja_forms_field_9_label_hidden" value="Üzenet *">
+                                  <div id="ninja_forms_field_9_error" style="display:none;" class="ninja-forms-field-error">
+                                  </div>
+                              </div>
 
-         </div> <!-- main-container  -->
-      </div> <!-- kapcsolat-form-cont -->
+                              <!-- submit -->
+                              <div class="field-wrap submit-wrap label-above" id="ninja_forms_field_6_div_wrap" data-visible="1">
+                                  <input type="hidden" id="ninja_forms_field_6_type" value="submit">
+                                  <div id="nf_submit_5">
+                                      <input type="submit" name="_ninja_forms_field_6" class="ninja-forms-field " id="ninja_forms_field_6" value="Elküld" rel="6">
+                                  </div>
+                                  <div id="nf_processing_5" style="display:none;">
+                                      <input type="submit" name="_ninja_forms_field_6" class="ninja-forms-field " id="ninja_forms_field_6" value="Processing" rel="6" disabled>
+                                  </div>
+                                  <div id="ninja_forms_field_6_error" style="display:none;" class="ninja-forms-field-error">
+                                  </div>
+                              </div>
+                           </div>
+                        </div>
+
+                     </form>
+                 </div>
+              </div>
+</div>
+            </div><!-- main-container  -->
+         </div> <!-- kapcsolat-form-cont -->
 
    </div><!-- kapcsolat-section -->
